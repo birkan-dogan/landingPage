2c646b2dd13aff878d903e07916756b4963bce6b
birkan <b.561birkan@gmail.com>
2023-12-09 22:31:05 +0300

    working on Services comp 🎯

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,30 @@
-"use client"
+"use client";
 
-import Bubbles from '@/components/Bubbles';
-import Hero from '@/components/Hero';
-import Image from 'next/image'
+import Bubbles from "@/components/Bubbles";
+import Hero from "@/components/Hero";
 import AOS from "aos";
 
-import { useEffect } from 'react';
-import Icons from '@/components/Icons';
-import About from '@/components/About';
-import Whyus from '@/components/WhyUs';
-import Services from '@/components/Services';
+import { useEffect } from "react";
+import Icons from "@/components/Icons";
+import About from "@/components/About";
+import Whyus from "@/components/WhyUs";
+import Services from "@/components/Services";
 
 export default function Home() {
-
-  useEffect(()=> {
+  useEffect(() => {
     AOS.init({
       duration: 2000,
     });
-  },[])
-  
+  }, []);
+
   return (
     <div>
-      <Bubbles/>
-      <Hero/>
-      <Icons/>
-      <About/>
+      <Bubbles />
+      <Hero />
+      <Icons />
+      <About />
       <Whyus />
-      {/* <Services /> */}
+      <Services />
     </div>
-    
-  )
+  );
 }
