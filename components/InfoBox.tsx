2cb0419b3fed0ff6438bea6e3471038e6616118a
birkan <b.561birkan@gmail.com>
2024-01-26 23:07:45 +0300

    🟢update

---
+++ components/InfoBox.tsx
@@ -0,0 +1,29 @@
+const InfoBox = () => {
+  return (
+    <div className="process-content-wrap bg-[#F5950B]">
+      <div className="process-sticky-wrap">
+        <div className="content-area-title content-left-align">
+          <div className="content-title-area auto-width-title-area">
+            <div className="content-title-wrap left-align-content-title">
+              <div className="my-[25rem] lgr:ml-9 lgg:ml-4">
+                <div className="le:h-[12rem] le:w-[27.5rem] border-4 border-[#1E1E1F] bg-white drop-shadow-[5px_4px_0px_rgb(30,30,31)]">
+                  <div className="h-[3.5rem] border-b-4 border-[#1E1E1F] bg-[#C1B4FB] m-auto flex items-center">
+                    <div className="w-[1.125rem] h-[1.125rem] bg-white rounded-full border-2 border-[#1E1E1F] ml-3" />
+                    <div className="w-[1.125rem] h-[1.125rem] bg-white rounded-full border-2 border-[#1E1E1F] ml-1" />
+                    <div className="w-[1.125rem] h-[1.125rem] bg-white rounded-full border-2 border-[#1E1E1F] ml-1" />
+                  </div>
+
+                  <h4 className="text-3xl font-extrabold m-[1.6rem] text-center justify-center items-center">
+                    Search Engine Optimization (seo)
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBox;
